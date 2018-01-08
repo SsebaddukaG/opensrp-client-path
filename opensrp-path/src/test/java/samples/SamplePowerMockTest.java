3f7147181b61bc2ec2c4528d8bc13e677cddbb30
Ephraim Kigamba <ekigamba@ona.io>
2018-01-08 15:50:40 +0300

    Add PowerMockRunnerDelegate, Robolectric & PowerMock configs

--- opensrp-path/src/test/java/samples/SamplePowerMockTest.java
+++ opensrp-path/src/test/java/samples/SamplePowerMockTest.java
@@ -3,12 +3,21 @@ package samples;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import io.ona.kujaku.BuildConfig;
 
 /**
  * Created by Ephraim Kigamba - ekigamba@ona.io on 08/01/2018.
  */
 @RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
 public class SamplePowerMockTest {
 
     @Test
